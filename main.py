0e15e75403f94ef9b2ebd50021732daf5ae2d6a5
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2025-04-10 19:24:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from PyQt6 import uic,QtWidgets
+import sqlite3
+
+def salvar_dados():
+    nome = tela_cadastro.txtNome.text()
+    telefone = tela_cadastro.txtTelefone.text()
+    endereco = tela_cadastro.txtEndereco.text()
+
+    try:
+        banco = sqlite3.connect('bd_oficina.db')
+        cursor = banco.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS Costureiro (NomeCostureiro text,TelefoneCostureiro text,Endereco text)')
+        cursor.execute("INSERT INTO COSTUREIRO (NomeCostureiro ,TelefoneCostureiro ,Endereco)VALUES (?,?,?)",(nome,telefone,endereco))
+        banco.commit()
+        banco.close()
+        print('Dados salvos com sucesso!')
+
+    except sqlite3.Error  as erro:
+        print(f'Erro ao inserir os dados: {erro}')
+
+
+
+
+app = QtWidgets.QApplication([])
+tela_cadastro=uic.loadUi("OficinaDeCostura.ui")
+tela_cadastro.pushButton.clicked.connect(salvar_dados)
+
+tela_cadastro.show()
+app.exec()
+# def salvar_dados():
+#     nome = txtEndereco.text()
