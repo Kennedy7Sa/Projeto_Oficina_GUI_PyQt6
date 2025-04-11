355241bd586dd33e346ff3a223a08edcc0a40d50
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2025-04-11 17:20:46 -0300

    read da tabela costureiro funcionando

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from PyQt6 import uic,QtWidgets
 import sqlite3
 
+def listar_costureiros():
+    try:
+        
+        banco = sqlite3.connect('bd_oficina.db')
+        cursor = banco.cursor() 
+        cursor.execute('SELECT * FROM Costureiro')
+        dados_lidos = cursor.fetchall()
+        tela_cadastro.tabelaCostureiro.setRowCount(len(dados_lidos))
+        tela_cadastro.tabelaCostureiro.setColumnCount(4)
+        for i in range(0, len(dados_lidos)):
+            for j in range(0,4):
+                tela_cadastro.tabelaCostureiro.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j]))) 
+        banco.close()
+
+
+    except sqlite3.Error  as erro:
+        print(f'Erro exibir dados: {erro}')
+
 def salvar_dados():
     nome = tela_cadastro.txtNome.text()
     telefone = tela_cadastro.txtTelefone.text()
     endereco = tela_cadastro.txtEndereco.text()
     endereco = tela_cadastro.txtEndereco.text()
+    listar_costureiros()
 
     try:
         banco = sqlite3.connect('bd_oficina.db')
@@ -25,7 +44,7 @@ def salvar_dados():
 app = QtWidgets.QApplication([])
 tela_cadastro=uic.loadUi("OficinaDeCostura.ui")
 tela_cadastro.pushButton.clicked.connect(salvar_dados)
-
+listar_costureiros()
 tela_cadastro.show()
 app.exec()
 # def salvar_dados():
